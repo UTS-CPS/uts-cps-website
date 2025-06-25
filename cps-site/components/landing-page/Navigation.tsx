3f4a2cfac7f50b4258550c48cf98ad5ed48f6178
ilias-vas <yakult1032@gmail.com>
2025-06-25 23:26:28 +1000

    update routing to nextjs spec and update favicon/page title

--- cps-site/components/landing-page/Navigation.tsx
+++ cps-site/components/landing-page/Navigation.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../ui/button";
 import { Terminal } from "lucide-react";
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navigation = () => {
   return (
@@ -16,30 +17,18 @@ const Navigation = () => {
           <span className="font-mono text-lg font-bold text-slate-100">UTS CPS</span>
         </div>
         <div className="hidden md:flex gap-6">
-          <a
-            href="#about"
-            className="text-slate-300 hover:text-indigo-300 transition-colors"
-          >
+         <Link href='#about' className="text-slate-300 hover:text-indigo-300 transition-colors">
             About
-          </a>
-          <a
-            href="#events"
-            className="text-slate-300 hover:text-indigo-300 transition-colors"
-          >
+         </Link>
+         <Link href='#events' className="text-slate-300 hover:text-indigo-300 transition-colors">
             Events
-          </a>
-          <a
-            href="#resources"
-            className="text-slate-300 hover:text-indigo-300 transition-colors"
-          >
+         </Link>
+         <Link href='/resources' className="text-slate-300 hover:text-indigo-300 transition-colors">
             Resources
-          </a>
-          <a
-            href="#join"
-            className="text-slate-300 hover:text-indigo-300 transition-colors"
-          >
+         </Link>
+         <Link href='#join' className="text-slate-300 hover:text-indigo-300 transition-colors">
             Join Us
-          </a>
+         </Link>
         </div>
       </nav>
     </header>
